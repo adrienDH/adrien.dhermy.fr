18da681378f5c6686805926eaeb6193355b2ee10
adrien <adrien.dhermy@gmail.com>
2016-02-06 01:43:01 +0100

    #Changement animation bouton

--- resources/js/home/home.js
+++ resources/js/home/home.js
@@ -1,16 +1,16 @@
 $( document ).ready(function() {
 	$("body").on("click",".js-contact", function() {
-		$("body").animate({ scrollTop: $("body").height()}, 1500);
+		$("body").animate({ scrollTop: $("body").height()}, 1000);
 		$(".js-target").focus();
 	});
 	
 	$("nav").on("click",".js-scroll-top", function() {
-		$("body").animate({ scrollTop: 0 }, 1500);
+		$("body").animate({ scrollTop: 0 }, 1000);
 	});
 	
 	$("nav").on("click",".js-scroll", function() {
 		var id = $(this).attr("data-scroll-target");
-		$("body").animate({ scrollTop: $(id).offset().top - 50}, 1500);
+		$("body").animate({ scrollTop: $(id).offset().top - 50}, 1000);
 	});
 	
 	$("#presentation").on("click",".js-more-infos", function() {
