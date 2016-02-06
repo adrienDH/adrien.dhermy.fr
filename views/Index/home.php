ae29e441f9010cb38d3b73978278d4bf15d7d81e
adrien <adrien.dhermy@gmail.com>
2016-02-06 01:01:14 +0100

    #Mise au propre de tous les blocs

--- views/Index/home.php
+++ views/Index/home.php
@@ -7,7 +7,12 @@
 <div id="presentation">
 	<div class="container">
 		<div class="row">
-			<div class="col-xs-12">
+			<div class="hidden-xs hidden-sm col-md-3">
+				<div class="picture-wrapper">
+					<img src="./resources/images/photo.png">
+				</div>
+			</div>
+			<div class="col-xs-12 col-md-9">
 				<h1 class="title">
 					Bonjour, Je m'appelle <spans class="name">Adrien Dhermy</span>
 				</h1>
@@ -33,33 +38,75 @@
 			<h3 class="subtitle">Informatique : </h3>
 			<div class="col-xs-12 col-md-4">
 				<div class="bloc">
-					<div class="year">2015</div>
-					ffff
+					<div class="year">Octobre 2014 à aujourd'hui</div>
+					<div>
+						<span class="jobtitle">Développeur Front</span> chez <span class="company">Jobijoba</span>
+					</div>
+					<div>
+						en <span class="contract">CDI</span>
+					</div>
+					<div class="text">Travail dans le pôle front en communication avec le les pôles marketing et Back End</div>
+					<div class="text">
+						Langage et compétence utilisés : 
+						<span class="skills">PHP</span>, 
+						<span class="skills">HTML</span>, 
+						<span class="skills">CSS</span>, 
+						<span class="skills">Jquery</span>, 
+						<span class="skills">Boostrap</span>, 
+						<span class="skills">MongoDB</span>
+					</div>
 				</div>
 			</div>
 			<div class="col-xs-12 col-md-4">
 				<div class="bloc">
-					<div class="year">2014</div>
-					ffff
+					<div class="year">Février à Juillet 2014</div>
+					<div>
+						<span class="jobtitle">Développeur Front</span> au <span class="company">CNRS</span>
+						<div>
+							en <span class="contract">Stage (+3 semaine en CDD)</span>
+						</div>
+						<div class="text">Travail avec les laborantin pour stocker des mesures dans une base de données</div>
+						<div class="text">
+							Langage et compétence utilisés : 
+							<span class="skills">PHP</span>, 
+							<span class="skills">HTML</span>, 
+							<span class="skills">CSS</span>, 
+							<span class="skills">SQL</span>, 
+							<span class="skills">PhpMyAdmin</span>
+						</div>
+					</div>
 				</div>
 			</div>
 			<div class="col-xs-12 col-md-4">
 				<div class="bloc">
-					<div class="year">2013</div>
-					ffff
+					<div class="year">Mai à Juin 2012</div>
+					<span class="jobtitle">Conseiller technique</span> chez <span class="company">iConcept</span>
+					<div>
+						en <span class="contract">Stage</span>
+					</div>
+					<div class="text">Travail avec les commerciaux et techniciens pour la vente de matériel Apple</div>
+					<div class="text">
+						Langage et compétence utilisés : 
+						<span class="skills">Préparation d'ordinateur</span>, 
+						<span class="skills">MAC OSX</span>,
+					</div>
 				</div>
 			</div>
+		</div>
+		<div class="row">
 			<h3 class="subtitle">Autres : </h3>
 			<div class="col-xs-12 col-md-6">
 				<div class="bloc">
-					<div class="year">2012</div>
-					ffff
+					<div class="year">Juillet - Août 2014</div>
+					<span class="jobtitle">Agent de service</span> chez <span class="company">Saines Nettoyage</span>
+					<div class="text">Entretien de plusieurs résidences</div>
 				</div>
 			</div>
 			<div class="col-xs-12 col-md-6">
 				<div class="bloc">
-					<div class="year">2011</div>
-					ffff
+					<div class="year">Vaccances 2009 à 2012</div>
+					<span class="jobtitle">Réserviste</span> dans <span class="company">l'Armée de l'air</span>
+					<div class="text">Sécurité et protection de la base 705 de Tours</div>
 				</div>
 			</div>
 		</div>
@@ -72,30 +119,30 @@
 		<div class="row">
 			<div class="col-xs-12 col-md-6">
 				<span class="bar">
-					<span class="full html"><span class="number">HTML</span></span>
+					<span class="full html"><span class="skill">HTML</span></span>
 				</span>
 				<span class="bar">
-					<span class="full php"><span class="number">PHP</span></span>
+					<span class="full css"><span class="skill">CSS</span></span>
 				</span>
 				<span class="bar">
-					<span class="full css"><span class="number">CSS</span></span>
+					<span class="full php"><span class="skill">PHP</span></span>
 				</span>
 				<span class="bar">
-					<span class="full html"><span class="number">HTML</span></span>
+					<span class="full jquery"><span class="skill">Jquery</span></span>
 				</span>
 			</div>
 			<div class="col-xs-12 col-md-6">
 				<span class="bar">
-					<span class="full html"><span class="number">HTML</span></span>
+					<span class="full bootstrap"><span class="skill">Bootstrap</span></span>
 				</span>
 				<span class="bar">
-					<span class="full php"><span class="number">PHP</span></span>
+					<span class="full sql"><span class="skill">SQL</span></span>
 				</span>
 				<span class="bar">
-					<span class="full css"><span class="number">CSS</span></span>
+					<span class="full linux"><span class="skill">Linux</span></span>
 				</span>
 				<span class="bar">
-					<span class="full html"><span class="number">HTML</span></span>
+					<span class="full mongodb"><span class="skill">MongoDB</span></span>
 				</span>
 			</div>
 		</div>
@@ -108,20 +155,26 @@
 		<div class="row">
 			<div class="col-xs-12 col-md-4">
 				<div class="circle">
-					<div class="year">2010</div>
-					<div class="text">text</div>
+					<div class="year">2014</div>
+					<div>
+						<span class="degree">Licence professionnelle SARI</span> à l'IUT de Bordeaux (Mention B) 
+					</div>
 				</div>
 			</div>
 			<div class="col-xs-12 col-md-4">
 				<div class="circle">
-					<div class="year">2010</div>
-					<div class="text">text</div>
+					<div class="year">2013</div>
+					<div>
+						<span class="degree">BTS IRIS</span> au Lycée Grandmont de Tours (Mention AB)
+					</div>
 				</div>
 			</div>
 			<div class="col-xs-12 col-md-4">
 				<div class="circle">
-					<div class="year">2010</div>
-					<div class="text">text</div>
+					<div class="year">2011</div>
+					<div>
+						<span class="degree">BAC STI</span> au Lycée de la Plaine de l'Ain de Amberieu en Bugey (Mention AB)
+					</div>
 				</div>
 			</div>
 		</div>
