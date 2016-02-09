30a2e50e608e49e7427d1266b42706da09bc26cb
adrien <adrien.dhermy@gmail.com>
2016-02-09 20:04:32 +0100

    #Ajout du header

--- inc/header.php
+++ inc/header.php
@@ -1,15 +1,17 @@
-<html>
-<title>Adrien Dhermy Développeur CV</title>
-<meta name="description" content="Retrouvez toutes mes informations professionnelles dans mon CV en ligne" />
-<meta name="keywords" content="Développeur, CV Développeur, adrien dhermy, Adrien Dhermy CV"> 
-
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-<link rel="stylesheet" type="text/css" href="./resources/bootstrap/css/bootstrap.css">
-<link rel="stylesheet" type="text/css" href="./resources/css/common/common.css">
-<link rel="stylesheet" type="text/css" href="./resources/icomoon/style.css">
-<link rel="icon" type="image/png" href="./resources/images/photo.png" />
-
+<!doctype html>
+<html lang="fr">
+<head>
+	<title>Adrien Dhermy Développeur CV</title>
+	<meta name="description" content="Retrouvez toutes mes informations professionnelles dans mon CV en ligne" />
+	<meta name="keywords" content="Développeur, CV Développeur, adrien dhermy, Adrien Dhermy CV"> 
+	
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
+	<link rel="stylesheet" type="text/css" href="./resources/bootstrap/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="./resources/css/common/common.css">
+	<link rel="stylesheet" type="text/css" href="./resources/icomoon/style.css">
+	<link rel="icon" type="image/png" href="./resources/images/photo.png" />
+</head>
 
 <?php foreach($mainController->getCssPaths() as $css): ?>
 	<?= "<link rel='stylesheet' type='text/css' href='" . $css .  ".css'>" ?>
