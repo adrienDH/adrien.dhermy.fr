e059d47ce88a5185a2143ed5f760c73cb525ea62
adrien <adrien.dhermy@gmail.com>
2016-02-20 12:49:19 +0100

    fix newsletter

--- ajax/add_email_newsletter.php
+++ ajax/add_email_newsletter.php
@@ -2,8 +2,6 @@
 
 include("../fonc/fonc.php");
 
-var_dump($_POST["email"]);die();
-
 $db = connexionSQL();
 $query = "INSERT INTO newsletter VALUES ('" . $_POST["email"] . "', '" . time() . "')";
 $db->query($query);
