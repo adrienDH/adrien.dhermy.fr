d04f70b1f6e94e2e87ab5e54c0be7ee6c51616f4
adrien <adrien.dhermy@gmail.com>
2016-10-17 19:14:20 +0200

    Suivi des users

--- inc/header.php
+++ inc/header.php
@@ -21,6 +21,16 @@
 	<?php foreach($mainController->getCssPaths() as $css): ?>
 		<?= "<link rel='stylesheet' type='text/css' href='" . $css .  ".css'>" ?>
 	<?php endforeach;?>
+	
+	<!-- Suivi des users avec smartlook.com -->
+	<script type="text/javascript">
+    	window.smartlook||(function(d) {
+    		var o=smartlook=function(){ o.api.push(arguments)},h=d.getElementsByTagName('head')[0];
+    		var c=d.createElement('script');o.api=new Array();c.async=true;c.type='text/javascript';
+   	 	c.charset='utf-8';c.src='//rec.smartlook.com/recorder.js';h.appendChild(c);
+    	})(document);
+    	smartlook('init', '3db75da17eb2838ca65f1b315ef0359b290106bc');
+	</script>
 </head>
 
 <body>
